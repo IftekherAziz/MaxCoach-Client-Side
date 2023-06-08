7f56d6d49e0e5c0b578887194cd248bfc8cc4226
Iftekher Aziz <azizchy02@gmail.com>
2023-06-08 22:46:06 +0600

    Add Icont to dashboard menu items

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,63 +1,97 @@
+// import Header from "../Pages/Shared/Header";
+// import Footer from "../Pages/Shared/Footer";
 import { Outlet } from "react-router-dom";
-import Header from "../Pages/Shared/Header";
-import Footer from "../Pages/Shared/Footer";
 import { NavLink } from "react-router-dom";
+import {
+  FaUsers,
+  FaSchool,
+  FaLandmark,
+  FaLayerGroup,
+  FaBookReader,
+  FaBookmark,
+  FaDonate,
+  FaHistory,
+  FaHome,
+} from "react-icons/fa";
 
 const DashboardLayout = () => {
   return (
     <div>
-      <Header></Header>
-      <div className="md:min-h-[calc(100vh-350px)] max-w-7xl mx-auto">
+      {/*  <Header></Header> */}
+      <div className="min-h-screen max-w-7xl mx-auto">
         <div className="drawer lg:drawer-open">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content text-center my-8">
+          <div className="drawer-content  py-5 border border-l-0 shadow-sm">
             {/* Page content here */}
-            <label
-              htmlFor="my-drawer"
-              className="border rounded mt-10 px-3 py-2 cursor-pointer bg-zinc-50  drawer-button lg:hidden "
-            >
-              Dashboard Menu
-            </label>
-            <div className="mt-8">
+            <div className="text-center">
+              <label
+                htmlFor="my-drawer"
+                className="border rounded mt-10 px-3 py-2 cursor-pointer bg-zinc-50  drawer-button lg:hidden "
+              >
+                Dashboard Menu
+              </label>
+            </div>
+
+            <div className="mt-8  mx-10">
               <Outlet></Outlet>
             </div>
-            
           </div>
           <div className="drawer-side">
             <label htmlFor="my-drawer" className="drawer-overlay"></label>
-            <ul className="menu p-4 w-80 h-full bg-zinc-50 text-black">
+            <ul className="menu p-4 w-60 h-full bg-zinc-50 shadow-xl text-black">
               {/* Sidebar content here */}
               <div className="text-2xl font-medium">Dashboard</div>
               <div className="divider"></div>
-              <li>
-                <NavLink to="/dashboard/manage-classes">Manage Classes</NavLink>
+              <li className="mb-1">
+                <NavLink to="/dashboard/manage-classes">
+                  <FaSchool></FaSchool>Manage Classes
+                </NavLink>
               </li>
-              <li>
-                <NavLink to="/dashboard/manage-users">Manage Users</NavLink>
-              </li>       
-              <li>
-                <NavLink to="/dashboard/add-class">Add A Class</NavLink>
-              </li>       
-              <li>
-                <NavLink to="/dashboard/my-classes">My Classes</NavLink>
-              </li>       
-              <li>
-                <NavLink to="/dashboard/enrolled-classes">Enrolled Classes</NavLink>
-              </li>       
-              <li>
-                <NavLink to="/dashboard/selected-classes">Selected Classes</NavLink>
-              </li>       
-              <li>
-                <NavLink to="/dashboard/payment">Make Payment</NavLink>
-              </li>       
-              <li>
-                <NavLink to="/dashboard/Payment-history">Payment History</NavLink>
-              </li>       
+              <li className="mb-1">
+                <NavLink to="/dashboard/manage-users">
+                  <FaUsers></FaUsers> Manage Users
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/dashboard/add-class">
+                  <FaLandmark></FaLandmark>Add A Class
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/dashboard/my-classes">
+                  <FaLayerGroup></FaLayerGroup>My Classes
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/dashboard/enrolled-classes">
+                  <FaBookReader></FaBookReader>Enrolled Classes
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/dashboard/selected-classes">
+                  <FaBookmark></FaBookmark>Selected Classes
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/dashboard/payment">
+                  <FaDonate></FaDonate>Make Payment
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/dashboard/Payment-history">
+                  <FaHistory></FaHistory>Payment History
+                </NavLink>
+              </li>
+              <li className="mb-1">
+                <NavLink to="/">
+                  <FaHome></FaHome>Back to Home
+                </NavLink>
+              </li>
             </ul>
           </div>
         </div>
       </div>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </div>
   );
 };
